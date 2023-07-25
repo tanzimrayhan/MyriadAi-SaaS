ef517c638902fa60686011789ad32d8864901345
Tanzimrayhan <Tanzim_rayhan@yahoo.com>
2023-07-25 15:55:29 -0400

    Added Route Groups

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const DashboardPage=()=> {
+  return (
+   <p>Dashboard Page</p>
+  )
+}
+
+export default DashboardPage ;
