a9d24b7a1a9571865e3fb35af269d3d191d1fc10
Tanzimrayhan <Tanzim_rayhan@yahoo.com>
2023-07-26 20:51:23 -0400

    added navbar, routing and sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,10 @@
 import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs';
 import Image from 'next/image'
 
 const DashboardPage = () => {
   return (
-  <>
+  
     <p>Dashboard Page</p>
-    <UserButton afterSignOutUrl='/' />
-  </>
   )
 }
 
